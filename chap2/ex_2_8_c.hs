b28e708b2372216fe7de42ef4b8c065902cde256
sughimura <nxsughi@gmail.com>
2025-12-06 22:08:57 +0900

    refactor: use :

--- chap2/ex_2_8_c.hs
+++ chap2/ex_2_8_c.hs
@@ -4,7 +4,7 @@ modernise :: String -> String
 modernise = unwords . map capitalise . words
 
 capitalise :: String -> String
-capitalise xs = [toUpper (head xs)] ++ tail xs
+capitalise xs = toUpper (head xs) : tail xs
 
 main = do
   putStrLn $ modernise "The morphology of prex ーan essay in meta-algorithmics"
