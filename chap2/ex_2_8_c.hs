a88df26aa3adcf8d13abf2ff0fcbc4862cfd9ef8
sughimura <nxsughi@gmail.com>
2025-12-06 18:19:25 +0900

    feat: add modernise

---
+++ chap2/ex_2_8_c.hs
@@ -0,0 +1,10 @@
+import Data.Char (toUpper)
+
+modernise :: String -> String
+modernise = unwords . map capitalise . words
+
+capitalise :: String -> String
+capitalise xs = [toUpper (head xs)] ++ tail xs
+
+main = do
+  putStrLn $ modernise "The morphology of prex ーan essay in meta-algorithmics"
