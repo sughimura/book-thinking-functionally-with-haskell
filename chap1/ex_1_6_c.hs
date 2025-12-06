b9c17392855454f192866f4414c2a2234bab81b1
sughimura <nxsughi@gmail.com>
2025-12-06 16:15:57 +0900

    feat: add ex 1.6c

---
+++ chap1/ex_1_6_c.hs
@@ -0,0 +1,9 @@
+main = do
+  print $ [1, 2, 3] ++ [3, 2, 1]
+  print [1, 2, 3, 3, 2, 1]
+  print $ "Hello" ++ " World!"
+  print "Hello World!"
+  print $ [1, 2, 3] ++ []
+  print [1, 2, 3]
+  print $ "Hello" ++ "" ++ "World!"
+  print "HelloWorld!"
