d02bcc355369abff68fc2fd7cf84d8fc61e00ade
sughimura <nxsughi@gmail.com>
2026-01-10 12:01:45 +0900

    feat: add ex_2_8_i.hs

---
+++ chap2/ex_2_8_i.hs
@@ -0,0 +1,15 @@
+import Data.Char (isAlpha, toLower)
+
+palindrome :: IO ()
+palindrome =
+  do
+    putStrLn "Enter a string:"
+    xs <- getLine
+    if isPalindrome xs
+      then putStrLn "Yes!"
+      else putStrLn "No!"
+
+isPalindrome :: String -> Bool
+isPalindrome xs = (ys == reverse ys)
+  where
+    ys = map toLower (filter isAlpha xs)
